a96cfea5bda7c806db9f42a5e57d8dbca3991c19
Narayana-Rao <narayanarao.bhogapurapu@gmail.com>
2025-04-24 12:19:56 -0700

    rlee folder update

--- build/lib.win-amd64-cpython-312/polsartools/preprocessing/filters.py
+++ build/lib.win-amd64-cpython-312/polsartools/preprocessing/filters.py
@@ -29,16 +29,15 @@ def process_chunk_boxcar(chunks, window_size, input_filepaths, *args):
 def rlee(infolder, outname=None, chi_in=0, psi_in=0, window_size=3, write_flag=True, max_workers=None):
     # File reading and output setup similar to the boxcar filter
     input_filepaths, output_filepaths = get_filter_io_paths(infolder, outname, window_size, filter_type="refined_lee")
-
-    # Process chunks in parallel
     num_outputs = len(output_filepaths)
+    
+    ### Python implementation
     # process_chunks_parallel(input_filepaths, output_filepaths, window_size=window_size, write_flag=write_flag,
     #                         processing_func=process_chunk_refined_lee, block_size=(512, 512), max_workers=max_workers,
     #                         num_outputs=num_outputs)
     
     #### Uncomment below to use C++ implementation 
-    ### Have to debug line by line in c++ code to make sure it is working correctly
-    ## As of now it is generating all zeros
+
     process_chunks_parallel(input_filepaths, output_filepaths, window_size=window_size, write_flag=write_flag,
                         processing_func=process_chunk_rfl, block_size=(512, 512), max_workers=max_workers,
                         num_outputs=num_outputs)
