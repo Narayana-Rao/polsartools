f09f45dd349edee1b304a3de193796a33f91da1f
Narayana-Rao <narayanarao.bhogapurapu@gmail.com>
2025-06-03 09:47:08 -0700

    Update utils.py

--- polsartools/utils/utils.py
+++ polsartools/utils/utils.py
@@ -29,7 +29,8 @@ def time_it(func):
     return wrapper
 
 def conv2d(a, f):
-    filt = np.zeros(a.shape).astype(np.complex64)
+    # filt = np.zeros(a.shape).astype(np.complex64)
+    filt = np.zeros(a.shape)
     wspad = int(f.shape[0]/2)
     s = f.shape + tuple(np.subtract(a.shape, f.shape) + 1)
     strd = np.lib.stride_tricks.as_strided
